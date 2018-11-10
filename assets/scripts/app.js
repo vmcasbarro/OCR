629bf8dc0bac4f39ea971042c2b49e0fa46bf7e4
vmcasbarro <vincenzo.casbarro@gmail.com>
2018-11-10 16:12:41 -0500

    allow user to select image and run OCR on it.
    - grab source from the img src when running tesseract

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -10,12 +10,10 @@ $(() => {
   // require the tesseract
   const Tesseract = require('tesseract.js')
 
-  // specify image
-  const image = '../../public/tesseract-sample-image.png'
-
   // function running the OCR
   const parse = function (event) {
     console.log('clicked!')
+    const image = $('#source-image').attr('src')
     Tesseract.recognize(image)
       .progress(function (message) { console.log('progress is: ', message) })
       .then(function (result) {
@@ -28,27 +26,19 @@ $(() => {
       })
   }
 
-  const show = function (event) {
+  // function replaces the demo image with a preview of the user-selected image
+  const showUserSelectedImage = function (event) {
     event.preventDefault()
-    $('#upload-image').removeClass('hidden')
-    $('#sample-image').addClass('hidden')
-
     if (event.target.files && event.target.files[0]) {
       const reader = new FileReader()
-
       reader.onload = function (e) {
-        $('#user-image')
-          .attr('src', e.target.result)
+        $('#source-image').attr('src', e.target.result)
       }
       reader.readAsDataURL(event.target.files[0])
     }
-
-    console.log('show image!', event.target.files)
   }
 
   // event listener for clicking the run button
   $('#OCR').on('click', parse)
-  $('#choose-photo').on('change', show)
-
-
+  $('#choose-photo').on('change', showUserSelectedImage)
 })
